ccb8f74b201f25ddda2b2184e7b349d5f816a90d
Soskretkov <oskrserg@gmail.com>
2022-07-03 22:25:33 +0300

    начал делать regexp для отсева каталогов с @

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use console::Term;
+use glob::{glob_with, MatchOptions};
 use std::env;
 use xlsxwriter::Workbook;
 #[macro_use]
 extern crate error_chain;
-extern crate walkdir;
+// extern crate walkdir;
+
 use walkdir::{DirEntry, WalkDir};
 mod extract;
 mod load;
@@ -15,22 +17,15 @@ use crate::transform::Act;
 
 error_chain! {
     foreign_links {
-        WalkDir(walkdir::Error);
-        Io(std::io::Error);
+        Glob(glob::GlobError);
+        Pattern(glob::PatternError);
     }
 }
 
 fn main() -> Result<()> {
-    let (path, sh_name) = ui::session();
-    // let sh_name = "Лист1".to_owned();
-    // let path = r"C:\Users\User\rust\acts_ks2_etl\";
-    fn is_not_temp(entry: &DirEntry) -> bool {
-        entry
-            .file_name()
-            .to_str()
-            .map(|s| !s.starts_with('~') && !s.contains('@'))
-            .unwrap_or(false)
-    }
+    // let (path, sh_name) = ui::session();
+    let sh_name = "лист1".to_owned();
+    let path = r"C:\Users\User\rust\acts_ks2_etl\";
     let report_name_prefix = env::args()
         .next()
         .unwrap()
@@ -40,48 +35,50 @@ fn main() -> Result<()> {
     let report_name = report_name_prefix + ".xlsx";
     let wb = Workbook::new(&report_name);
     let mut report = Report::new(wb);
+    let options = MatchOptions {
+        case_sensitive: false,
+        ..Default::default()
+    };
 
-    for entry in WalkDir::new(path)
-        .into_iter()
-        .filter_map(|e| e.ok()) //будет молча пропускать каталоги, на доступ к которым у владельца запущенного процесса нет разрешения
-        .filter(is_not_temp)
-    {
-        let f_path = entry.path().to_string_lossy();
+    for entry in glob_with(r"**/*/[@]*.xlsm", options)? {
+        // let f_path = entry.unwrap().as_path().to_string_lossy().to_string();
+        let f_path = entry?.to_string_lossy().to_string();
 
-        if f_path.ends_with(".xlsm") {
-            let mut file = Book::new(&f_path.to_string()).unwrap();
+        let mut file = Book::new(&f_path).unwrap();
 
-            let sheet = Sheet::new(
-                &mut file,
-                &sh_name,
-                &SEARCH_REFERENCE_POINTS,
-                29, //передается для расчета смещения столбцов. Это сумма номеров столбцов Y-типа в DESIRED_DATA_ARRAY: 0 + 0 + 3 + 5 + 9 + 9 + 3.
-            )
-            .unwrap_or_else(|err| {
-                if let Some(text) = ks2_etl::error_message(err, &sh_name) {
-                    let _ = Term::stdout().clear_screen();
-                    println!("\nВозникла ошибка. \n{}", text);
-                    println!("\nФайл, вызывающий ошибку: {}", f_path);
-                    loop {}
-                };
-                panic!()
-            });
-
-            let act = Act::new(sheet).unwrap_or_else(|err| {
+        let sheet = Sheet::new(
+            &mut file,
+            &sh_name,
+            &SEARCH_REFERENCE_POINTS,
+            29, //передается для расчета смещения столбцов. Это сумма номеров столбцов Y-типа в DESIRED_DATA_ARRAY: 0 + 0 + 3 + 5 + 9 + 9 + 3.
+        )
+        .unwrap_or_else(|err| {
+            if let Some(text) = ks2_etl::error_message(err, &sh_name) {
                 let _ = Term::stdout().clear_screen();
-                println!("\nВозникла ошибка. \n{}", err);
-                println!("\nФайл, вызывающий ошибку: {}", f_path);
-                loop {}
-            });
-
-            if let Err(err) = report.write(&act) {
-                let _ = Term::stdout().clear_screen();
-                println!("\nВозникла ошибка. \n{}", err);
+                println!("\nВозникла ошибка. \n{}", text);
                 println!("\nФайл, вызывающий ошибку: {}", f_path);
                 loop {}
             };
-        }
-        println!("Успешно собрана информация из {} актов", report.empty_row - 1);
+            panic!()
+        });
+
+        let act = Act::new(sheet).unwrap_or_else(|err| {
+            let _ = Term::stdout().clear_screen();
+            println!("\nВозникла ошибка. \n{}", err);
+            println!("\nФайл, вызывающий ошибку: {}", f_path);
+            loop {}
+        });
+
+        if let Err(err) = report.write(&act) {
+            let _ = Term::stdout().clear_screen();
+            println!("\nВозникла ошибка. \n{}", err);
+            println!("\nФайл, вызывающий ошибку: {}", f_path);
+            loop {}
+        };
+        println!(
+            "Успешно собрана информация из {} актов",
+            report.empty_row - 1
+        );
     }
     let number_of_files = report.empty_row - 1;
     let file = report.end();
